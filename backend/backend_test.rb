3b8a2d93608de09328303d5c3ee28192febcf4b0
Shutkov Maksim <shutkov.maksim@mail.ru>
2018-10-19 12:28:52 +0200

    Max implemenatations

--- backend/backend_test.rb
+++ backend/backend_test.rb
@@ -1,3 +1,7 @@
+require 'date'
+
+PRICE_LIST = { first_checkin: 10, last_checkout: 5, checkout_checkin: 10 }
+
 input = {
   "listings": [
     { "id": 1, "num_rooms": 2 },
@@ -18,7 +22,40 @@ input = {
 }
 
 def handle_cleanings(input)
-  # Write your code here
+  result = [];
+
+  input[:listings].each do |room|
+    room_booking = parse_dates(input[:bookings].select{|owner_room| owner_room[:listing_id] == room[:id]})
+    room_reservations = parse_dates(input[:reservations].select{|reserv| reserv[:listing_id] == room[:id]})
+
+    room_booking.each do |booking|
+      booking_reservations = room_reservations.select{|reserv| check_crossing_period(booking, reserv)}
+      result.push(create_mission(room, booking[:start_date], "first_checkin"));
+      result.push(create_mission(room, booking[:end_date], "last_checkout"));
+
+      booking_reservations.each do |reserv|
+        result.push(create_mission(room, reserv[:end_date], "checkout_checkin")) if reserv[:end_date] != booking[:end_date];
+      end
+    end
+  end
+  {"missions" => result}
+end
+
+def parse_dates(entities)
+  entities.map do |entity|
+    entity[:parsed_start_date] = Date.parse(entity[:start_date]);
+    entity[:parsed_end_date] = Date.parse(entity[:end_date]);
+    entity[:period] = Date.parse(entity[:start_date])..Date.parse(entity[:end_date])
+    entity
+  end
+end
+
+def check_crossing_period(booking, reserv)
+  reserv[:parsed_end_date] > reserv[:parsed_start_date] && booking[:period].include?(reserv[:parsed_start_date]) && booking[:period].include?(reserv[:parsed_end_date])
+end
+
+def create_mission(listing, date, type)
+  {listing_id: listing[:id], mission_type: type, date: date, price: PRICE_LIST[type.to_sym] * listing[:num_rooms]}
 end
 
 p handle_cleanings(input)
