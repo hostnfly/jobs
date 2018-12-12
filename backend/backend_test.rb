2253c6b758a94a610668b89118d9e95c50fedc1f
Melissasat <melissa.grach@live.fr>
2018-12-12 22:37:20 +0100

    generate a JSON of all the mission and their mission_type, date, and price

--- backend/backend_test.rb
+++ backend/backend_test.rb
@@ -1,3 +1,6 @@
+PRICE = { first_checkin: 10, last_checkout: 5, checkout_checkin: 10 }
+
+
 input = {
   "listings": [
     { "id": 1, "num_rooms": 2 },
@@ -17,8 +20,38 @@ input = {
   ]
 }
 
+def get_last_checkout(id, date, bookings)
+  bookings.each do |booking|
+      return false if booking[:listing_id] == id && booking[:end_date] == date
+  end
+  return true
+end
+
 def handle_cleanings(input)
-  # Write your code here
+  listings = input[:listings]
+  bookings = input[:bookings]
+  reservations = input[:reservations]
+  result = []
+  
+  
+  listings.each do |listing|
+    # for each listing's booking i put new missions in result
+    bookings.each do |booking|
+      if listing[:id] == booking[:listing_id]
+        result <<  {:listing_id => booking[:listing_id], :mission_type=> 'first_checkin', :date=> booking[:start_date], :price=> PRICE[:first_checkin] * listing[:num_rooms]}
+        result <<  {:listing_id => booking[:listing_id], :mission_type=> 'last_checkout', :date=> booking[:end_date], :price=> PRICE[:last_checkout] * listing[:num_rooms]}
+      end
+    end
+    # for each reservation,i make a checkout_checkin if last_checkout is missing
+    reservations.each do |reservation|
+      if listing[:id] == reservation[:listing_id] && get_last_checkout(reservation[:listing_id], reservation[:end_date], bookings)
+        result <<  {:listing_id => reservation[:listing_id], :mission_type=> 'checkout_checkin', :date=> reservation[:end_date], :price=> PRICE[:checkout_checkin] * listing[:num_rooms]}
+      end
+    end
+  end
+
+  return {"missions": result}
+  
 end
 
 p handle_cleanings(input)
